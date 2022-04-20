7164a3250f507b49bfbf622065f1b8c698a2c3df
unknown <shaikfurkhan1998@gmail.com>
2022-04-20 14:21:51 +0530

    ok

---
+++ build/precache-manifest.fb9d670d668609f67037d5be225b6a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c12ce0dcfe2129cf284ec4cc896f412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796b79c8519731f5dc15",
+    "url": "/static/css/main.d567319b.chunk.css"
+  },
+  {
+    "revision": "61c589e695dbb76f07b4",
+    "url": "/static/js/2.0777a2b6.chunk.js"
+  },
+  {
+    "revision": "796b79c8519731f5dc15",
+    "url": "/static/js/main.44c7081b.chunk.js"
+  },
+  {
+    "revision": "08f26478a427e66c5937",
+    "url": "/static/js/runtime~main.e18a9abf.js"
+  },
+  {
+    "revision": "d63cd00370a02c44e53a97e3db3668a1",
+    "url": "/static/media/wall.d63cd003.png"
+  }
+]);
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("bookjs/build"))
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "bookjs", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
 })
 
 mongoose.connect("mongodb+srv://shaikfurkhan1998:"+encodeURIComponent("&Haik#123")+"@cluster0.024ix.mongodb.net/booksdb");
